f49918a48a656c84647cc3ba56461ea09b311004
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-10-25 22:03:41 +0200

    max3function
    implementations of a max3 function

---
+++ Haskell/basics/max3function.hs
@@ -0,0 +1,25 @@
+--implementations of a max function with 3 params
+-- disclaimer: again no checks for cases like negative inputs etc.
+-- naive with if clauses:
+-- this pre def with types isn't needed in haskell, but might help the programmer understand
+max3 :: Int -> Int -> Int -> Int
+max3 x y z
+  | (x >= y && x >= z) = x
+  | (y>=x && y >= z) = y
+  | (z>= x && z >= y) = z
+  | otherwise = error "not possible"
+
+max3Guards x y z
+  | x >= y && x >= z = x
+  | y >=x && y >= z = y
+  | z >= x && z >= y = z
+  | otherwise = error "not possible"
+
+-- shorter implementation that uses recursion
+maxR x y z = if x > y && x > z then x else maxR z x y
+
+-- implementation using the max(x,y) function of hakell
+maxC x y z = max x $ max y z
+
+
+
