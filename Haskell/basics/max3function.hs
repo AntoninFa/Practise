ef23cae5bf37834c7f827d6b033d630cc9467c46
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-10-25 22:07:10 +0200

    Update max3function.hs

--- Haskell/basics/max3function.hs
+++ Haskell/basics/max3function.hs
@@ -3,11 +3,7 @@
 -- naive with if clauses:
 -- this pre def with types isn't needed in haskell, but might help the programmer understand
 max3 :: Int -> Int -> Int -> Int
-max3 x y z
-  | (x >= y && x >= z) = x
-  | (y>=x && y >= z) = y
-  | (z>= x && z >= y) = z
-  | otherwise = error "not possible"
+max3 x y z = if(x > y && x > z) then x else if (y>x && y > z) then y else if (z> x && z > y) then z else error "not possible" 
 
 max3Guards x y z
   | x >= y && x >= z = x
@@ -15,7 +11,7 @@ max3Guards x y z
   | z >= x && z >= y = z
   | otherwise = error "not possible"
 
--- shorter implementation that uses recursion
+-- shorter and more elegant implementation that uses recursion
 maxR x y z = if x > y && x > z then x else maxR z x y
 
 -- implementation using the max(x,y) function of hakell
