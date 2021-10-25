a752a7fbd12db149a42189b9e18de935fafbe031
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-10-25 23:34:16 +0200

    Update defineAFunction.hs

--- Haskell/basics/defineAFunction.hs
+++ Haskell/basics/defineAFunction.hs
@@ -1,3 +1,7 @@
+import GHC.Num.Backend (bignat_powmod_word)
 -- defining simple mathematical functions like f(x) = sin(x) / x
 f x = sin x / x
 g x = x * f(x*x)
+-- ghci> mult 4 3 
+-- 12
+mult a b = a*b 
