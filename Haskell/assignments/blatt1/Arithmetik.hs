f2b175e467cd90daa03ef5c2d9b86a441df09199
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-10-27 21:24:18 +0200

    pow2 works
    Maybe check if it is equal to assignment def

--- Haskell/assignments/blatt1/Arithmetik.hs
+++ Haskell/assignments/blatt1/Arithmetik.hs
@@ -5,7 +5,6 @@ pow1 b 0 = 1
 pow1 b e = b * pow1 b (e-1)   
 
 pow2 b 0 = 1
-pow2 b 2 = b*b
-pow2 b e = if ((e `mod` 2) == 0) then (b * (pow2 b (e `div` 2))) else (b* (pow2 b (e-1)))
-
--- 4 4 -> 4* (pow 4 2)-> 4*4* (pow 4 1)-> 4*4*4*1
+pow2 0 e = 0
+pow2 b 1 = b
+pow2 b e = if ((e `mod` 2) == 0) then (pow2 b (div e 2))* (pow2 b (div e 2)) else b* (pow2 b (e-1))
