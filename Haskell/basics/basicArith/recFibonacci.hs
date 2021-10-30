291d3ab5f075f6c81390978cc4f79b4a38d8257b
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-10-30 23:40:26 +0200

    fibo with Akku
    not yet tested

--- Haskell/basics/basicArith/recFibonacci.hs
+++ Haskell/basics/basicArith/recFibonacci.hs
@@ -1,6 +1,20 @@
 
--- fibonacci number
-fib 0 = 0
-fib 1 = 1
-fib 2 = 1
-fib n = (fib (n-1)) + (fib (n-2))
+-- fibonacci number, naive impl
+fibN 0 = 0
+fibN 1 = 1
+fibN n = (fib (n-1)) + (fib (n-2))
+
+-- rek. Aufrufe O(2^n), fib erzeugt Aufrufbaum 
+-- (Ein Baum der tiefe h hat 2^(h-1) Blätter und 2^t Knoten
+
+-- fibbo mit Akku:
+
+fibAcc n n1 n2
+ | (n== 0) =n1
+ | (n== 1) = n2
+ | otherwise = fibAcc (n-1) n2 (n1 * n2)
+
+fib n = fibAcc n 0 1
+
+-- O(n) rekursive Aufrufe 
+-- Endrekursion + Akku ~ while + lokale Variable
