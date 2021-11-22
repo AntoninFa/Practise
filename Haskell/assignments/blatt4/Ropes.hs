0a1184cdf133682ad621a2a85714512f3a970c02
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-11-22 13:29:57 +0100

    haskell neues Blatt und sol4

--- Haskell/assignments/blatt4/Ropes.hs
+++ Haskell/assignments/blatt4/Ropes.hs
@@ -13,3 +13,6 @@ ropeConcat :: Rope a -> Rope a -> Rope a
 ropeConcat (Leaf a) (Inner l i r) = (Inner (Leaf a) i (Inner l i r))
 ropeConcat (Inner l i r) r2 = Inner l i (ropeConcat r r2)
 
+MusterropeConcat :: Rope a -> Rope a -> Rope a
+MusterropeConcat l r = Inner l (ropeLength l) r 
+
