bd2d07291a55aff570b6f59714090709fd492cfb
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2021-11-22 20:22:15 +0100

    Update Ropes.hs

--- Haskell/assignments/blatt4/Ropes.hs
+++ Haskell/assignments/blatt4/Ropes.hs
@@ -13,6 +13,15 @@ ropeConcat :: Rope a -> Rope a -> Rope a
 ropeConcat (Leaf a) (Inner l i r) = (Inner (Leaf a) i (Inner l i r))
 ropeConcat (Inner l i r) r2 = Inner l i (ropeConcat r r2)
 
-MusterropeConcat :: Rope a -> Rope a -> Rope a
-MusterropeConcat l r = Inner l (ropeLength l) r 
+musterropeConcat :: Rope a -> Rope a -> Rope a
+musterropeConcat l r = Inner l (ropeLength l) r
+
+ropeSplitAt :: Int-> Rope a -> (Rope a, Rope a)
+ropeSplitAt i (Leaf a) = (Leaf (take i a), Leaf(drop i a))
+ropeSplitAt i (Inner l w r) 
+     | (i < w) = let (ll, lr) = ropeSplitAt i l     -- let sagt, dass das Tuper(ll,lr) den Inhalt aus ropeSplitAt i l und das wird dann im in genutzt-> in ist der Ergb. Tupel
+                   in (ll, ropeConcat lr r)
+     | (i > w) = let (rl, rr) = ropeSplitAt (i-w) r
+                   in (ropeConcat l rl, rr)
+     | otherwise = (l,r)
 
