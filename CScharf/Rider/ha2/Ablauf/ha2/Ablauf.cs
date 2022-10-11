da0a3185034d6f86f1d27380cc8aa7bb3964c610
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-10-11 23:49:40 +0200

    yes

---
+++ CScharf/Rider/ha2/Ablauf/ha2/Ablauf.cs
@@ -0,0 +1,217 @@
+﻿// Ablauf.cs (zu V02-Personenverzeichnis)
+
+using Daten;
+
+namespace Ablauf;
+
+internal class Program
+{
+    // wirft Ausnahme falls Bedingung (condition) zutrifft
+    private static void ExceptionOn(bool condition)
+    {
+        if (condition)
+            throw new Exception();
+    }
+
+    // Vergleich der Angaben einer Person mit Vorgaben
+    private static void PersonVergleichen(Person person, string name, string straßenname, int hausnummer)
+    {
+        ExceptionOn(person.Name != name
+                    || person.Adresse.Straße.Name != straßenname
+                    || person.Adresse.Hausnummer != hausnummer);
+    }
+
+    // Ausgabe einer Person auf Console
+    private static void PersonDrucken(Person person)
+    {
+        Console.WriteLine($"{person.Name}, {person.Adresse.Straße.Name} {person.Adresse.Hausnummer}");
+    }
+
+    private static void Main()
+    {
+        // einige korrekte Objekte
+
+        var straße1 = new Straße("General Graf von Doppelwumms-Platz");
+        var straße2 = new Straße("Blumendorfer Landstraße");
+
+        var adresse1 = new Adresse(straße1, 2);
+        var adresse2 = new Adresse(straße1, 3);
+        var adresse3 = new Adresse(straße2, 4);
+
+        Person person1 = new Person("Peters, Hans", adresse1);
+        Person person2 = new Person("Meier, Sybille", adresse1);
+        Person person3 = new Person("Schmitz, Hubert", adresse2);
+        Person person4 = new Person("Müller, Katrin", adresse3);
+        Person person5 = new Person("Hufnagel, Susanne", adresse3);
+
+        PersonDrucken(person1);
+        PersonDrucken(person2);
+        PersonDrucken(person3);
+        PersonDrucken(person4);
+        PersonDrucken(person5);
+
+        Console.WriteLine();
+
+        // Änderungen an der Straße führt auch zur Änderung an Adresse und Person
+        // Änderung an Adresse führt auch zur Änderung an Person
+
+        straße1.Name = "Bertha Blumendorf-Platz";
+        adresse3.Hausnummer = 14;
+
+        PersonDrucken(person1);
+        PersonDrucken(person2);
+        PersonDrucken(person3);
+        PersonDrucken(person4);
+        PersonDrucken(person5);
+
+        Console.WriteLine();
+
+        PersonVergleichen(person1, "Peters, Hans", "Bertha Blumendorf-Platz", 2);
+        PersonVergleichen(person2, "Meier, Sybille", "Bertha Blumendorf-Platz", 2);
+        PersonVergleichen(person3, "Schmitz, Hubert", "Bertha Blumendorf-Platz", 3);
+        PersonVergleichen(person4, "Müller, Katrin", "Blumendorfer Landstraße", 14);
+        PersonVergleichen(person5, "Hufnagel, Susanne", "Blumendorfer Landstraße", 14);
+
+        // Änderungsversuche an Person sollen wirkungslos sein
+
+        person1.Adresse.Hausnummer = 42;
+        person2.Adresse.Straße.Name = "Edward Snowden-Platz";
+
+        PersonVergleichen(person1, "Peters, Hans", "Bertha Blumendorf-Platz", 2);
+        PersonVergleichen(person2, "Meier, Sybille", "Bertha Blumendorf-Platz", 2);
+
+        // Änderungsversuche an der Straße über die Adresse sollen wirkungslos sein
+
+        adresse1.Straße.Name = "Edward Snowden-Platz";
+        ExceptionOn(adresse1.Straße.Name != "Bertha Blumendorf-Platz");
+
+        // Kopien sollen tief sein
+
+        var straße1Kopie = new Straße(straße1);
+        Adresse adresse2Kopie = adresse2.Kopie();
+        straße1.Name = "No Name-Platz";
+        adresse2.Hausnummer = 211;
+        ExceptionOn(straße1Kopie.Name != "Bertha Blumendorf-Platz");
+        ExceptionOn(adresse2Kopie.Hausnummer != 3);
+        ExceptionOn(adresse2Kopie.Straße.Name != "Bertha Blumendorf-Platz");
+
+        // Plausiprüfs
+
+        var anzahlFehler = 0;
+
+        try
+        {
+            new Straße((string)null);
+            Console.WriteLine("Fehler 1");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Straße(" ");
+            Console.WriteLine("Fehler 2");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Straße((Straße)null);
+            Console.WriteLine("Fehler 3");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        var teststraße = new Straße("Name");
+        try
+        {
+            teststraße.Name = null;
+            Console.WriteLine("Fehler 4");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            teststraße.Name = " ";
+            Console.WriteLine("Fehler 5");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Adresse(null, 1);
+            Console.WriteLine("Fehler 6");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Adresse(teststraße, 0);
+            Console.WriteLine("Fehler 7");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        var testadresse = new Adresse(teststraße, 1);
+        try
+        {
+            testadresse.Hausnummer = 0;
+            Console.WriteLine("Fehler 8");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Person(null, testadresse);
+            Console.WriteLine("Fehler 9");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Person(" ", testadresse);
+            Console.WriteLine("Fehler 10");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            new Person("Name", null);
+            Console.WriteLine("Fehler 11");
+            anzahlFehler++;
+        }
+        catch
+        {
+        }
+
+        if (anzahlFehler > 0)
+            throw new Exception();
+    }
+}
---
+++ CScharf/Rider/ha2/Ablauf/ha2/Daten.cs
@@ -0,0 +1,33 @@
+﻿//Daten.cs (zu V02-Personenverzeichnis)
+
+namespace Daten;
+
+internal class Straße
+{
+    private string _name;
+
+    public Straße(string name)
+    {
+        _name = name;
+    }
+
+    public Straße(Straße straße)
+    {
+        if (straße == null) throw new ArgumentNullException();
+        _name = straße._name;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (value == null) throw new ArgumentNullException();
+            _name = value;
+        }
+    }
+}
+
+internal class Adresse
+{
+}
