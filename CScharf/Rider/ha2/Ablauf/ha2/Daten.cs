bd96f2c74a023537d9989b482f43f927599b65ec
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-10-14 17:08:01 +0200

    1707
    Noch Fehler 02 und 10

--- CScharf/Rider/ha2/Ablauf/ha2/Daten.cs
+++ CScharf/Rider/ha2/Ablauf/ha2/Daten.cs
@@ -2,14 +2,22 @@
 
 namespace Daten;
 
-internal class Straße
+class Straße
 {
     private string _name;
 
     public Straße(string name)
     {
-        _name = name;
+        if (!String.IsNullOrEmpty(name))
+        {
+            _name = name;
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
     }
+
 
     public Straße(Straße straße)
     {
@@ -17,17 +25,97 @@ internal class Straße
         _name = straße._name;
     }
 
+
     public string Name
     {
         get => _name;
         set
         {
-            if (value == null) throw new ArgumentNullException();
-            _name = value;
+            if (!String.IsNullOrWhiteSpace(value))
+            {
+                _name = value;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            
         }
     }
 }
 
-internal class Adresse
+class Adresse
 {
+    private Straße _straße;
+    private int _hausnummer;
+
+    public Straße Straße
+    {
+        get { return new Straße(_straße); }
+    }
+
+    public int Hausnummer
+    {
+        get { return _hausnummer; }
+
+        set
+        {
+            if (value > 0)
+            {
+                _hausnummer = value;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+
+    public Adresse(Straße straße, int hausnummer)
+    {
+        if (straße != null && hausnummer > 0)
+        {
+            this._straße = straße;
+            this._hausnummer = hausnummer;
+        }
+        else
+        {
+            throw new ArgumentNullException();
+        }
+    }
+
+    public Adresse Kopie()
+    {
+        return new Adresse(new Straße(_straße), _hausnummer);
+    }
+}
+
+class Person
+{
+    private string _name;
+    private Adresse _adresse;
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public Adresse Adresse
+    {
+        get { return _adresse.Kopie(); }
+    }
+
+    public Person(string name, Adresse adresse)
+    {
+        if (name != null && adresse != null)
+        {
+            this._name = name;
+            this._adresse = adresse;
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
 }
