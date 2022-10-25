ab3f1a4cf2cb2d66b2f4e4c7d3ce8e2d68ef4317
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-10-25 09:32:18 +0200

    graphen m2

---
+++ cSharp/p1/GraphCounter/GraphCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Haupt
+{
+    static void Main()
+    {
+        int[] vertices = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        string edges = "";
+        char[] edgesArray = edges.ToCharArray();
+        
+    }
+
+}
+
+public class Tupel
+{
+    public int A;
+    public int B;
+
+    Tupel(int a, int b)
+    {
+        this.A = a;
+        this.B = b;
+    }
+}
