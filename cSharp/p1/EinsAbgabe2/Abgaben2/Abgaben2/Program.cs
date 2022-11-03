4b910d6901ac06c49a72bc921c3938bd5b4e4aa1
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-11-03 16:24:59 +0100

    String rev Works

---
+++ cSharp/p1/EinsAbgabe2/Abgaben2/Abgaben2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace Abgaben2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var gnirts = "abcdefghijklmnop";
+            var sW = new StringWorker();
+            Console.Write(sW.Reverse(gnirts));
+            
+        }
+    }
+
+    class StringWorker
+    {
+        public string Reverse(string wert)
+        {
+            if (wert.Length <= 1)
+                return wert[0].ToString();
+            
+            return Reverse(lawnMower(wert, (int)wert.Length/2))  + "" +    Reverse(guillotine(wert, ((int)wert.Length/2)));
+        }
+
+        public string guillotine(string s, int last)
+        {
+            var bob = new StringBuilder("", last);
+            var stringAsChars = s.ToCharArray();
+            var headAsChars = new char[last];
+            for (int i = 0; i < last; i++)
+            {
+                headAsChars[i] = stringAsChars[i];
+            }
+
+            bob.Append(headAsChars);
+            return bob.ToString();
+        }
+
+        private String lawnMower(string s, int first)
+        {
+            var bob = new StringBuilder("", s.Length-first);
+            var stringAsChars = s.ToCharArray();
+            var tailAsChars = new char[s.Length - first];
+            for (int i = first; i < s.Length; i++)
+            {
+                tailAsChars[i-first] = stringAsChars[i];
+            }
+
+            bob.Append(tailAsChars);
+            return bob.ToString();
+        }
+    }
+    
+    
+    
+}
+
