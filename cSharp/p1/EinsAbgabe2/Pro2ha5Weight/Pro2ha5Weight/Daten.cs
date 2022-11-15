22b1f23db0e903c14f804b77337b9978c5d151e3
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-11-15 01:26:02 +0100

    abst elek Waage erstellt

--- cSharp/p1/EinsAbgabe2/Pro2ha5Weight/Pro2ha5Weight/Daten.cs
+++ cSharp/p1/EinsAbgabe2/Pro2ha5Weight/Pro2ha5Weight/Daten.cs
@@ -60,4 +60,61 @@ namespace Daten
         }
         
     }
+    public class Last : IGewichtHabend
+    {
+        private double _gewicht;
+        public double Gewicht
+        {
+            get => _gewicht;
+        }
+    }
+    public abstract class ElektrischeWaage
+    {
+        private Last _last;
+        private double _spannung;
+
+        public double Spannung
+        {
+            get => _spannung;
+        }
+
+        public void Auflegen(Last last)
+        {
+            if (_last != null)
+                throw new Exception("voll");
+            this._last = last;
+        }
+
+        public void Wegnehmen()
+        {
+            if (_last == null)
+                throw new Exception("leer");
+            this._last = null;
+        }
+        
+        public override string ToString()
+        {
+            if (_last == null)
+                return $"Elektrische Waage, Leer, Spannung: {_spannung}";
+            return $"Elektrische Waage, Last: {_last.Gewicht}, Spannung: {_spannung}";
+        }
+
+        public override int GetHashCode()
+        {
+            return _last.GetHashCode() ^ _spannung.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Last l = obj as Last;
+            ElektrischeWaage eW = obj as ElektrischeWaage;
+            if (l != null )
+                return eW != null && _last.Gewicht.Equals(eW._last.Gewicht) && _spannung.Equals(eW.Spannung);
+            return eW != null && _spannung.Equals(eW.Spannung);
+        }
+        
+        
+    }
+    
+    
 }
