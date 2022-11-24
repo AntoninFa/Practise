fbbc7baee1a73850a314b8aa5b1772729ceb9fb1
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-11-24 14:35:17 +0100

    neues zul file

---
+++ CScharf/zulassung/TestDatenverarbeitung/Datenhaltung/Transfer/Infrastruktur_DUMMY.cs
@@ -0,0 +1,9 @@
+﻿// Infrastruktur_DUMMY.cs (zu Zulassungsaufgabe 22W)
+
+namespace EasyBankingBilanz.Datenhaltung.Transfer
+{
+    public record Infrastruktur(int AnzahlFilialen,
+                                Währung AnschaffungskostenJeFiliale,
+                                Währung ITWert,
+                                Währung ITInvestitionen);
+}
