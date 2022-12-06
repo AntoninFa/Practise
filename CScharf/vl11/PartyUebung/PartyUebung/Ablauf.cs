aa988ac8d7f21283ea7dcc5a749986e9776400d7
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2022-12-06 14:53:08 +0100

    Pary Uebung

---
+++ CScharf/vl11/PartyUebung/PartyUebung/Ablauf.cs
@@ -0,0 +1,98 @@
+﻿// Ablauf.cs (zu V11-Party)
+
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Ablauf
+{
+    class Person
+    {
+        public string Name { get; }
+
+        public Person(string name) => Name = String.IsNullOrWhiteSpace(name) ? throw new ArgumentNullException() : name;
+    }
+
+    class Party
+    {
+        private static Random _zufallsgenerator = new Random();
+        public Person Gastgeber { get; }
+
+        public Party(Person gastgeber)
+        {
+            Gastgeber = gastgeber ?? throw new ArgumentNullException(nameof(gastgeber));
+            Console.WriteLine($"Willkommen zur Party von {gastgeber.Name}!\n");
+        }
+
+        private void GetränkMixen(Person gast, string getränk)
+        {
+            Console.WriteLine($"Bitte ein(e) {getränk} für {gast.Name}!");
+            Task.Delay(_zufallsgenerator.Next(500, 1500)).Wait();
+            Console.WriteLine($"{getränk} für {gast.Name} ist " +
+                $"fertig!");
+        }
+
+        private void GastgeberBegrüßen(Person gast)
+        {
+            //TODO warum ging um Methodenaufruf nicht?
+            // Damit immer nur eine Person den Gastgeber begrüßt
+            lock (Gastgeber)
+            {
+                Console.WriteLine($"{gast.Name} begrüßt Gastgeber/in {Gastgeber.Name}!");
+                Task.Delay(_zufallsgenerator.Next(500, 1500)).Wait();
+                Console.WriteLine($"{gast.Name} ist fertig mit der Begrüßung von {Gastgeber.Name}!");
+            }
+        }
+
+        private void GetränkGenießen(Person gast, string getränk)
+        {
+            Console.WriteLine($"{gast.Name} genießt ihre(n)/seine(n) {getränk}!");
+            Task.Delay(_zufallsgenerator.Next(500, 1500)).Wait();
+        }
+
+        public void Besuchen(Person gast, string getränk)
+        {
+            _ = gast ?? throw new ArgumentNullException(nameof(gast));
+            _ = String.IsNullOrWhiteSpace(getränk) ? throw new ArgumentNullException(nameof(getränk)) : getränk;
+
+            Console.WriteLine($"Gast {gast.Name} kommt zur Party von {Gastgeber.Name}!");
+
+            GetränkMixen(gast, getränk);
+
+            GastgeberBegrüßen(gast);
+            
+            
+            GetränkGenießen(gast, getränk);
+
+            Console.WriteLine($"Gast {gast.Name} verlässt die Party von {Gastgeber.Name}!");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Party party = new Party(new Person("Claudia"));
+            List<Task> gäste = new List<Task>();
+
+            foreach ((string Person, string Getränk) gast in new (string, string)[]
+            {
+                ("Paul", "Bora Bora"),
+                ("Ella", "Pink Lemonade Margarita"),
+                ("Anne", "Chai Bubble Tea Latte"),
+                ("Jonathan", "Raspberry Lemonade")
+            })
+            {
+                Task.Delay(500).Wait();
+                //Task.Run(()=>party.Besuchen(new Person(gast.Person), gast.Getränk)); ohne Liste haben wir das Problem, dass 
+                // unser Program beim erreichen von the party ist over vom ersten Thread schon durch ist
+                gäste.Add(Task.Run(()=>party.Besuchen(new Person(gast.Person), gast.Getränk)));
+            }
+
+            // Wait all will nen Array und keine Liste
+            Task.WaitAll(gäste.ToArray());
+            Console.WriteLine("\nthe party is over!");
+        }
+    }
+}
