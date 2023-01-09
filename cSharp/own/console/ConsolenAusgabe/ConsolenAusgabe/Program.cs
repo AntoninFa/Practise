5958412477cb19dace657a5df6408142d8b47a96
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-01-09 14:39:22 +0100

    iter zeugs

---
+++ cSharp/own/console/ConsolenAusgabe/ConsolenAusgabe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Haupt
+{
+    public static void Main()
+    {
+
+        //private List<int> neu = new List<int>() { -8, 5 };
+        char c = '1';
+
+        Console.Write((int)'F');
+
+    }
+}
