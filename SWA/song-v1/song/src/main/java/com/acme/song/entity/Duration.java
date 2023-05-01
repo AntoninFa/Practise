8a9ead6f94d0743e2ecfb613aa343f8b399d5f44
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-05-01 15:58:43 +0200

    Restraints für Die Entities

--- SWA/song-v1/song/src/main/java/com/acme/song/entity/Duration.java
+++ SWA/song-v1/song/src/main/java/com/acme/song/entity/Duration.java
@@ -1,12 +1,15 @@
 package com.acme.song.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 /**
- * Klasse Speichert eine Zeitdauer.
+ * Klasse speichert eine Zeitdauer.
  * In Stunden:Minuten:Sekunden.
  */
 @Builder
@@ -15,8 +18,16 @@ import lombok.ToString;
 @ToString
 public class Duration {
 
+    @NotNull
+    @Min(0)
     private int hours;
+    @NotNull
+    @Min(0)
+    @Max(60)
     private int minutes;
+    @NotNull
+    @Min(0)
+    @Max(60)
     private int seconds;
 
     /**
@@ -25,6 +36,6 @@ public class Duration {
     @JsonIgnore
     @ToString.Exclude
     // Darf nicht @NotNull sein,
-    // weil beim Anlegen ueber REST der Rueckwaertsverweis noch nicht existiert
+    // weil beim Anlegen über REST der Rückwärts verweis noch nicht existiert
     private Song song;
 }
--- SWA/song-v1/song/src/main/java/com/acme/song/entity/Song.java
+++ SWA/song-v1/song/src/main/java/com/acme/song/entity/Song.java
@@ -1,10 +1,9 @@
 package com.acme.song.entity;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -19,38 +18,40 @@ import java.util.UUID;
 @ToString
 public class Song {
 
-
     /**
-     * ID die eine Song eindeutig identifiziert.
+     * ID die einen Song eindeutig identifiziert.
      */
     private UUID id;
-
 
     /**
      * Titel des Songs.
      */
+    @NotNull
+
     private String titel;
 
     /**
-     * Datum an dem der Song offiziell erschienen ist.
+     * Datum, an dem der Song offiziell erschienen ist.
      */
     @Past
     private LocalDate erscheinungsDatum;
 
-
     /**
      * Liste an Genres des Songs.
      */
+    @UniqueElements
+    @ToString.Exclude
     private List<GenreType> genre;
 
-
     /**
-     * Musik-Label unter welchem der SOng erschienen ist.
+     * Musik-Label, unter welchem der SOng erschienen ist.
      */
     private String musikLabel;
 
     /**
      * Dauer des Songs in Stunden:Minute:Sekunden.
      */
+    @Valid
+    @ToString.Exclude
     private Duration duration;
 }
