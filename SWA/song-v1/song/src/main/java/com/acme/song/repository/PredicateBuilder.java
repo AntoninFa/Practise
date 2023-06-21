5975ad77ab8df58ecf21e269993c8712d127a230
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-06-21 02:06:30 +0200

    Repository angepasst

---
+++ SWA/song-v1/song/src/main/java/com/acme/song/repository/PredicateBuilder.java
@@ -0,0 +1,88 @@
+package com.acme.song.repository;
+import com.acme.song.entity.GenreType;
+import com.acme.song.entity.QSong;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import static java.util.Locale.GERMAN;
+
+/**
+ * Singleton-Klasse, um Prädikate durch QueryDSL für eine WHERE-Klausel zu bauen.
+ */
+@Component
+@Slf4j
+public class PredicateBuilder {
+    /**
+     * Prädikate durch QueryDSL für eine WHERE-Klausel bauen.
+     *
+     * @param queryParams als MultiValueMap
+     * @return Predicate in QueryDSL für eine WHERE-Klausel
+     */
+    @SuppressWarnings("ReturnCount")
+    public Optional<Predicate> build(final Map<String, ? extends List<String>> queryParams) {
+        log.debug("build: queryParams={}", queryParams);
+
+        final var qSong = QSong.song;
+        final var booleanExprList = queryParams
+            .entrySet()
+            .stream()
+            .map(entry -> toBooleanExpression(entry.getKey(), entry.getValue(), qSong))
+            .toList();
+        if (booleanExprList.isEmpty() || booleanExprList.contains(null)) {
+            return Optional.empty();
+        }
+
+        final var result = booleanExprList
+            .stream()
+            .reduce(booleanExprList.get(0), BooleanExpression::and);
+        return Optional.of(result);
+    }
+
+    @SuppressWarnings({"CyclomaticComplexity", "UnnecessaryParentheses"})
+    private BooleanExpression toBooleanExpression(
+        final String paramName,
+        final List<String> paramValues,
+        final QSong qSong
+    ) {
+        log.trace("toSpec: paramName={}, paramValues={}", paramName, paramValues);
+
+        if (paramValues == null || (!Objects.equals(paramName, "genre") && paramValues.size() != 1)) {
+            return null;
+        }
+
+        final var value = paramValues.get(0);
+        return switch (paramName) {
+            case "titel" -> titel(value, qSong);
+            case "interesse" -> genre(paramValues, qSong);
+            default -> null;
+        };
+    }
+
+    private BooleanExpression titel(final String teil, final QSong qSong) {
+        return qSong.titel.toLowerCase().matches("%" + teil.toLowerCase(GERMAN) + '%');
+    }
+
+    private BooleanExpression genre(final Collection<String> genre, final QSong qSong) {
+        if (genre == null || genre.isEmpty()) {
+            return null;
+        }
+        final var expressionList = genre
+            .stream()
+            .map(genreStr -> GenreType.of(genreStr).orElse(null))
+            .filter(Objects::nonNull)
+            .map(g -> qSong.genreStr.matches("%" + g.name() + '%'))
+            .toList();
+        if (expressionList.isEmpty()) {
+            return null;
+        }
+        return expressionList
+            .stream()
+            .reduce(expressionList.get(0), BooleanExpression::and);
+    }
+}
--- SWA/song-v1/song/src/main/java/com/acme/song/repository/SongRepository.java
+++ SWA/song-v1/song/src/main/java/com/acme/song/repository/SongRepository.java
@@ -2,8 +2,11 @@ package com.acme.song.repository;
 
 import com.acme.song.entity.Song;
 import com.acme.song.entity.GenreType;
+import com.querydsl.core.types.Predicate;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -24,8 +27,7 @@ import static com.acme.song.repository.DB.SONGS;
  * Repository für den DB-Zugriff.
  */
 @Repository
-@Slf4j
-public class SongRepository {
+public interface SongRepository extends JpaRepository<Song, UUID>, QueryPredicateExecutor<Song> {
 
     /**
      * Einen Song anhand seiner ID suchen.
@@ -33,87 +35,25 @@ public class SongRepository {
      * @param id Die Id des gesuchten Songs.
      * @return Optional leer oder mit dem gefundenen Song.
      */
-    public Optional<Song> findById(final UUID id) {
-        log.debug("findById: id={}", id);
-        final var result = SONGS.stream()
-            .filter(song -> Objects.equals(song.getId(), id))
-            .findFirst();
-        log.debug("findById: {}", result);
-        return result;
-    }
+    @Override
+    Optional<Song> findById(UUID id);
 
     /**
-     * Alle Songs als Collection aus DB.
+     * Alle Songs als Liste aus DB.
      *
      * @return Alle Songs.
      */
-    public @NonNull Collection<Song> findAll() {
-        return SONGS;
-    }
+    @Override
+    List<Song> findAll();
 
     /**
-     * Einen neuen Song anlegen.
+     * Alle Songs anhand von Prädikat suchen.
      *
-     * @param song Der anzulegende Song
-     * @return Den Song.
+     * @return Alle Songs die das Prädikat erfüllen.
      */
-    public @NonNull Song create(final @NonNull Song song) {
-        log.debug("create: {}", song);
-        song.setId(randomUUID());
-        SONGS.add(song);
-        log.debug("create: {}", song);
-        return song;
-    }
+    @Override
+    List<Song> findAll(Predicate predicate);
 
-    /**
-     * Einen vorhandenen Song aktualisieren.
-     *
-     * @param song Das Song-Objekt mit den neuen Daten
-     */
-    public void update(final @NonNull Song song) {
-        log.debug("update: {}", song);
-        final OptionalInt index = IntStream
-            .range(0, SONGS.size())
-            .filter(i -> Objects.equals(SONGS.get(i).getId(), song.getId()))
-            .findFirst();
-        log.trace("update: index={}", index);
-        if (index.isEmpty()) {
-            return;
-        }
-        SONGS.set(index.getAsInt(), song);
-        log.debug("update: {}", song);
-    }
-
-    /**
-     * Kunden anhand einer Liste von Suchkriterien finden.
-     *
-     * @param suchkriterien Die Suchkriterien
-     * @return Liste aus Songs die zu den Suchkriterien passen, oder eine Leere Liste, falls keine passen
-     */
-    @SuppressWarnings({"ReturnCount", "JavadocLinkAsPlainText"})
-    public @NonNull Collection<Song> find(final Map<String, ? extends List<String>> suchkriterien) {
-        log.debug("find: suchkriterien ={}", suchkriterien);
-
-        if (suchkriterien.isEmpty()) {
-            return findAll();
-        }
-
-        for (final var e : suchkriterien.entrySet()) {
-            switch (e.getKey()) {
-                case "titel" -> {
-                    return findByTitel(e.getValue().get(0));
-                }
-                case "genre" -> {
-                    return findByGenre(e.getValue());
-                }
-                default -> {
-                    log.debug("find: Suchkriterium nicht erkannt: {}", e.getKey());
-                    return emptyList();
-                }
-            }
-        }
-        return emptyList();
-    }
 
     /**
      * Songs anhand seines Titels finden.
@@ -122,35 +62,12 @@ public class SongRepository {
      * @return den gesuchten Song oder eine leere Collection, falls kein passender
      *          Song gefunden wurde
      */
-    public @NonNull Collection<Song> findByTitel(final CharSequence titel) {
-        log.debug("findByTitel: titel={}", titel);
-
-        final var songs = SONGS.stream()
-            .filter(song -> song.getTitel().contains(titel))
-            .toList();
-        log.debug("findByTitel: titel={}", titel);
-        return songs;
-    }
-
-    @NonNull Collection<Song> findByGenre(final Collection<String> genreString) {
-        log.debug("findByGenre: genreString={}", genreString);
-        final var genres = genreString
-            .stream()
-            .map(genre -> GenreType.of(genre)
-                .orElse(null))
-            .toList();
-        if (genres.contains(null)) {
-            return emptyList();
-        }
-        final var songs = SONGS.stream()
-            .filter(song -> {
-                @SuppressWarnings("SetReplaceableByEnumSet")
-                final Collection<GenreType> songGenre = new HashSet<>(song.getGenre());
-                return songGenre.containsAll(genres);
-            })
-            .toList();
-        log.debug("findByGenre: songs={}", songs);
-        return songs;
-    }
+    @Query("""
+        SELECT   s
+        FROM     Song s
+        WHERE    lower(s.titel) LIKE concat('%', lower(:titel), '%')
+        ORDER BY s.id
+        """)
+    Collection<Song> findByTitel(String titel);
 
 }
